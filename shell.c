6145f8290025f9ed3ca3e14864ba7e1c3f0f2cf2
Teshi Waruingi <71100831+teshiwaruingi@users.noreply.github.com>
2022-12-13 18:49:53 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -11,8 +11,6 @@
 #define MAX_COMMAND_LINE_LEN 1024
 #define MAX_COMMAND_LINE_ARGS 128
 
-
-  
 char prompt[1024];
 char delimiters[] = " \t\r\n=|";
 extern char **environ;
@@ -27,28 +25,44 @@ void timerHandler(int signum) {
 
 int main() {
     // Stores the string typed into the command line.
-    char command_line[MAX_COMMAND_LINE_LEN];
-    char cmd_bak[MAX_COMMAND_LINE_LEN];
-    
-  
-    // Stores the tokenized command line input.
-    char *arguments[MAX_COMMAND_LINE_ARGS];
-    	
-    while (true) {
-        do{ 
-            getcwd(prompt, sizeof(prompt));
-            // Print the shell prompt.
-            printf("%s>", prompt);
-            fflush(stdout);
+char command_line[MAX_COMMAND_LINE_LEN];
 
-            // Read input from stdin and store it in command_line. If there's an
-            // error, exit immediately. (If you want to learn more about this line,
-            // you can Google "man fgets")
-        
-            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
-                fprintf(stderr, "fgets error");
-                exit(0);
-            }
+// Stores the tokenized command line input.
+char *arguments[MAX_COMMAND_LINE_ARGS];
+
+// Loop indefinitely until the user exits the shell.
+while (true) {
+  // Print the shell prompt.
+  printf("%s>", prompt);
+  fflush(stdout);
+
+  // Read input from stdin and store it in command_line. If there's an error,
+  // exit immediately.
+  if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+    fprintf(stderr, "fgets error");
+    exit(0);
+  }
+
+  // If the user input was EOF (ctrl+d), exit the shell.
+  if (feof(stdin)) {
+    printf("\n");
+    fflush(stdout);
+    fflush(stderr);
+    return 0;
+  }
+
+  // Tokenize the command line input into the arguments array.
+  char *token = strtok(command_line, delimiters);
+  int i = 0;
+  while (token != NULL) {
+    arguments[i] = token;
+    i++;
+    token = strtok(NULL, delimiters);
+  }
+
+  // Check the first argument to determine the command to execute.
+ 
+
  
         }while(command_line[0] == 0x0A);  // while just ENTER pressed
 
@@ -153,22 +167,31 @@ int main() {
           exit(1);
         }
         else {
-           if (!background_process) {
-              signal(SIGINT, exitSignalHandler);
-              signal(SIGALRM, timerHandler);
-           if (pipeProcess){
-             char *args2[] = {arguments[2], arguments[3], NULL};
-              dup2(pipefd[1], 1);
-              close(pipefd[0]);
-              execvp(arguments[2], args2);
-           }
-              alarm(10);
-           do {
-              wpid = waitpid(child_pid, &status, WUNTRACED);
-          } while (!WIFEXITED(status) && !WIFSIGNALED(status));};
-        }
-      }
+  // Check if a background process is not running
+  if (!background_process) {
+    // Set up signal handlers for SIGINT and SIGALRM
+    signal(SIGINT, exitSignalHandler);
+    signal(SIGALRM, timerHandler);
+
+    // Check if a pipe process is running
+    if (pipeProcess) {
+      // Set up arguments for the pipe process
+      char *args2[] = {arguments[2], arguments[3], NULL};
+      // Duplicate the write end of the pipe to stdout
+      dup2(pipefd[1], 1);
+      // Close the read end of the pipe
+      close(pipefd[0]);
+      // Execute the pipe process
+      execvp(arguments[2], args2);
     }
+    // Set up an alarm for 10 seconds
+    alarm(10);
+    // Wait for the child process to exit or be signaled
+    do {
+      wpid = waitpid(child_pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+}
     // This should never be reached.
     return -1;
 }
